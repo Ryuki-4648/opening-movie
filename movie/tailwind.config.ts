98a3ccd2094c137a1b4a01fd51e05b6371b77451
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2024-10-05 13:34:41 +0900

    scssに変更、font family追加

--- movie/tailwind.config.ts
+++ movie/tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        ten: ["Ten Mincho", "serif"],
+        cabin: ["Cabin", "sans-serif"],
+      },
     },
   },
   plugins: [],
