fdd6896bfeb3dc9c3bcbf719a286df588774bc06
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2024-10-08 22:16:55 +0900

    Swiper位置調節

--- movie/src/app/page.tsx
+++ movie/src/app/page.tsx
@@ -1,101 +1,135 @@
+"use client";
+
+import { Autoplay, EffectFade } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+// import styles from "./index.module.css";
+
+import history01 from "../../public/data/prod/history01.json";
+import history02 from "../../public/data/prod/history02.json";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+  return (
+    <div className="">
+      <main className="l-main overflow-auto h-screen w-full">
+
+        <section className="l-section h-screen w-full bg-black flex items-center">
+          <h1 className="text-[220px] text-white font-ten">It&apos;s the start of<br />Our Wedding<br />Reception</h1>
+        </section>
+
+        <section className="l-section h-screen w-full bg-gray-300 flex items-center">
+          <div className="max-w-[1400px] mx-auto">
+            <h2></h2>
+            <video className="video c-video01" src="/movie/dummy/movie_dummy01.mp4" autoPlay loop muted></video>
+          </div>
+        </section>
+
+        <section className="l-section h-screen w-full bg-gray-300 flex items-center">
+          <div className="max-w-[1400px] mx-auto">
+            <video className="video c-video01" src="/movie/dummy/movie_dummy02.mov" autoPlay loop muted></video>
+          </div>
+        </section>
+
+        <section className="l-section h-screen w-full bg-red-300 flex items-center">
+          <div className="max-w-[1400px] mx-auto">
+            <video className="video c-video01" src="/movie/dummy/movie_dummy03.mp4" autoPlay loop muted></video>
+          </div>
+        </section>
+
+        <section className="l-section h-screen w-full bg-red-300 flex items-center">
+          <div className="max-w-[1400px] mx-auto">
+            <video className="video c-video01" src="/movie/dummy/movie_dummy01.mp4" autoPlay loop muted></video>
+          </div>
+        </section>
+
+        <section className="l-section h-screen w-full bg-red-300 relative">
+          <div className="">
+            <h2 className="text-[180px] font-bold font-ten absolute top-0 left-0 leading-[0.6em]">GROOM</h2>
+            <div className="w-[500px] absolute right-0 top-0 h-full">
+              <Swiper
+                className="flex justify-end"
+                modules={[Autoplay, EffectFade]}
+                slidesPerView={1}
+                loop={true}
+                speed={100}
+                autoplay={{
+                  delay: 500,
+                  disableOnInteraction: false,
+                }}
+                // className={styles.slideWrapper}
+              >
+                {history01.map(({ id, image }) => (
+                  <SwiperSlide key={id}>
+                    <Image
+                      src={image}
+                      width={500}
+                      height={800}
+                      alt="Slider Image"
+                    />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+          </div>
+        </section>
+
+        <section className="l-section h-screen w-full bg-green-300 relative">
+          <div className="">
+            <h2 className="text-[180px] font-bold font-ten absolute top-0 left-0 leading-[0.6em]">BRIDE</h2>
+            <div className="w-[500px] absolute right-0 top-0 h-full">
+              <Swiper
+                  modules={[Autoplay]}
+                  slidesPerView={1}
+                  loop={true}
+                  speed={100} // スライドが切り替わる時の速度
+                  autoplay={{
+                    delay: 500,
+                    disableOnInteraction: false,
+                  }}
+                  // className={styles.slideWrapper}
+                >
+                  {history02.map(({ id, image }) => (
+                    <SwiperSlide key={id}>
+                      <Image
+                        src={image}
+                        width={500}
+                        height={300}
+                        alt="Slider Image"
+                      />
+                    </SwiperSlide>
+                  ))}
+              </Swiper>
+            </div>
+          </div>
+        </section>
+
+        <section className="l-section h-screen w-full bg-green-300">
+          <div className="">
+            <h2>hoge</h2>
+          </div>
+          
+        </section>
+
+        <section className="l-section h-screen w-full bg-blue-300">
+          <h2>hoge</h2>
+          <div className="bg-white w-[500px] mx-auto rounded-md p-4">
+            <p>ERROR</p>
+            <p>text</p>
+          </div>
+        </section>
+
+
+        <section className="l-section h-screen w-full flex items-center justify-center bg-gray-300">
+          <div className="">
+            <h2 className="text-[200px] font-ten">Are you ready?</h2>
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
     </div>
   );
 }
