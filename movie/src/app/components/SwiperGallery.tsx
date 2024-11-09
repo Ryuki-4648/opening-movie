7e9016f3543a4b41c1fa3a80e8fbeca7145049b8
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2024-11-09 23:23:08 +0900

    Swiper修正、autoplayflg追加

--- movie/src/app/components/SwiperGallery.tsx
+++ movie/src/app/components/SwiperGallery.tsx
@@ -10,28 +10,30 @@ import { TargetData } from "../types/targetData";
 
 interface SwiperGalleryProps {
   targetData: TargetData[];
+  autoplayflg: boolean;
 }
 
-export default function SwiperGallery({ targetData }: SwiperGalleryProps) {
+export default function SwiperGallery({ targetData, autoplayflg }: SwiperGalleryProps) {
   return (
     <Swiper
-    className="flex justify-end"
-    modules={[Autoplay, EffectFade]}
-    slidesPerView={1}
-    loop={true}
-    speed={100}
-    autoplay={{
-      delay: 500,
-      disableOnInteraction: false,
-    }}
-    // className={styles.slideWrapper}
-  >
+      className="flex justify-end"
+      modules={[Autoplay, EffectFade]}
+      slidesPerView={1}
+      loop={true}
+      speed={800}
+      autoplay={autoplayflg ? {
+        delay: 1500,
+        disableOnInteraction: false,
+      } : false}
+      // className={styles.slideWrapper}
+    >
     {targetData.map(({ id, image }) => (
       <SwiperSlide key={id}>
         <Image
           src={image}
-          width={500}
-          height={800}
+          width={600}
+          height={900}
+          className="rounded-tr-[80px]"
           alt="Slider Image"
         />
       </SwiperSlide>
