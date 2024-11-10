1ff3efc5ed63a66a3c0c554bf167f6fdc8c81f14
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2024-11-10 15:03:22 +0900

    Swiperコンポーネント修正

--- movie/src/app/components/SwiperHistory.tsx
+++ movie/src/app/components/SwiperHistory.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Autoplay, EffectFade } from "swiper/modules";
+import { Autoplay, FreeMode } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -8,34 +8,37 @@ import "swiper/css/pagination";
 import Image from 'next/image';
 import { TargetData } from "../types/targetData";
 
-interface SwiperGalleryProps {
+interface SwiperHistoryProps {
   targetData: TargetData[];
-  autoplayflg: boolean;
 }
 
-export default function SwiperGallery({ targetData, autoplayflg }: SwiperGalleryProps) {
+export default function SwiperHistory({ targetData }: SwiperHistoryProps) {
   return (
     <Swiper
-      key={autoplayflg ? 'autoplay-enabled' : 'autoplay-disabled'}
       className="flex justify-end"
-      modules={[Autoplay, EffectFade]}
-      slidesPerView={1}
+      modules={[Autoplay, FreeMode]}
+      slidesPerView="auto"
+      spaceBetween={20}
       loop={true}
-      speed={300}
-      autoplay={autoplayflg ? {
-        delay: 1200,
+      speed={1200}
+      autoplay={{
+        delay: 0,
         disableOnInteraction: false,
-      } : false}
+      }}
+      freeMode={{
+        enabled: true,
+        sticky: true, // スライドが止まることなくスムーズに流れ続ける
+      }}
       // className={styles.slideWrapper}
     >
     {targetData.map(({ id, image }) => (
       <SwiperSlide key={id}>
         <Image
           src={image}
-          width={600}
-          height={900}
-          className="rounded-tr-[100px]"
-          alt="Slider Image"
+          width={300}
+          height={500}
+          className=""
+          alt="Slider History Image"
         />
       </SwiperSlide>
     ))}
